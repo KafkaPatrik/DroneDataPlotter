ed914ce84a8256e3161ab004553a803f234daf79
Patrik Kafka <patrikafka@gmail.com>
2023-11-27 21:28:13 +0200

    Update Login Title

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ActivityIndicator,TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { FIREBASE_AUTH } from '../../Firebase/Firebase';
@@ -8,44 +8,44 @@ import { StatusBar } from 'expo-status-bar';
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loading,setLoading] = useState(false);
-  const auth=FIREBASE_AUTH;//or getAuth();
+  const [loading, setLoading] = useState(false);
+  const auth = FIREBASE_AUTH; // or getAuth();
 
-   //const response = await auth.signIn
-       // console.log(response);
-   // }navigation.navigate('Home'); 
- const signIn = async () => {
-  setLoading(true);
-    signInWithEmailAndPassword(auth,email,password)
-    .then((userCredential)=>{
-      setLoading(false);
-      const user=userCredential.user;
-      navigation.navigate('Home');
-    })
-   .catch((error)=>{
-    console.log(error);
-    alert('Sign in failed: '+ error.message);
-    setLoading(false);
-  });
-}
+  const signIn = async () => {
+    setLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setLoading(false);
+        const user = userCredential.user;
+        navigation.navigate('Home');
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Sign in failed: ' + error.message);
+        setLoading(false);
+      });
+  }
+
   const signUp = async () => {
     setLoading(true);
-    createUserWithEmailAndPassword(auth,email,password)
-    .then((userCredential)=>{
-      setLoading(false);
-      alert('Check your mails!');
-      const user=userCredential.user;
-      navigation.navigate('Home');
-    })
-   .catch((error)=>{
-    console.log(error);
-    alert('Registration failed: '+ error.message);
-    setLoading(false);
-   });
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setLoading(false);
+        alert('Check your mails!');
+        const user = userCredential.user;
+        navigation.navigate('Home');
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Registration failed: ' + error.message);
+        setLoading(false);
+      });
   }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+      <Text style={styles.title}>Drone Plot</Text>
+      <Text style={styles.subtitle}>Real-Time Drone Insights</Text>
       <TextInput
         style={styles.input}
         placeholder="Email"
@@ -59,16 +59,16 @@ const LoginScreen = ({ navigation }) => {
         onChangeText={(text) => setPassword(text)}
         value={password}
       />
-      {loading? (<ActivityIndicator size="large" color="#0000ff"/>
-      ):( <>
-          <TouchableOpacity style={styles.button} onPress={signIn}>
-            <Text style={styles.buttonText}>Login</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={signUp}>
-            <Text style={styles.buttonText}>Create Account</Text>
-          </TouchableOpacity>
+      {loading ? (<ActivityIndicator size="large" color="#0000ff" />
+      ) : (<>
+        <TouchableOpacity style={styles.button} onPress={signIn}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={signUp}>
+          <Text style={styles.buttonText}>Create Account</Text>
+        </TouchableOpacity>
       </>)
-    }
+      }
     </View>
   );
 };
@@ -82,6 +82,11 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 38,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 50,
   },
